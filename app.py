5088929916c41cb917e306512ea84b9d2860fc04
Raína Souza <raiina.py@gmail.com>
2024-08-27 16:21:01 -0300

    CRUD application

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
+
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'rainalinda'  
+jwt = JWTManager(app)
 
 def conn_db():
     return sqlite3.connect('zoonation.db')
@@ -12,21 +17,79 @@ def init_db():
     cursor.execute(
         '''
             CREATE TABLE IF NOT EXISTS animals(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            breed TEXT,
-            age INTEGER,
-            description TEXT NOT NULL,
-            city TEXT NOT NULL,
-            contact TEXT NOT NULL
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                breed TEXT,
+                age INTEGER,
+                description TEXT NOT NULL,
+                city TEXT NOT NULL,
+                contact TEXT NOT NULL
             )
         '''
         )
+    cursor.execute(
+        '''
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+            )
+        '''
+    )
     conn.commit()
     conn.close()
+
     
 init_db()
 
+# users
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Nome de usuário e senha são necessários!'}), 400
+
+    hashed_password = generate_password_hash(password)
+    
+    conn = conn_db()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, hashed_password))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        return jsonify({'message': 'Nome de usuário já existe!'}), 400
+    finally:
+        conn.close()
+
+    return jsonify({'message': 'Usuário registrado com sucesso!'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Nome de usuário e senha são necessários!'}), 400
+
+    conn = conn_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user and check_password_hash(user[2], password):
+        access_token = create_access_token(identity={'username': username})
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({'message': 'Credenciais inválidas!'}), 401
+    
+
+# animals
 @app.route('/pets',methods=['POST'])
 def add_pet():
     new_pet = request.get_json()
@@ -70,6 +133,37 @@ def read_pets():
 
     return jsonify(pets)
 
+@app.route('/pets/<int:pet_id>', methods=['PUT'])
+def update_animal(pet_id):
+    data = request.json
+    name = data.get('name')
+    breed = data.get('breed')
+    age = data.get('age')
+    description = data.get('description')
+    city = data.get('city')
+    contact = data.get('contact')
+
+    conn = conn_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+            UPDATE animals
+            SET name = ?, breed = ?, age = ?, description = ?, city = ?, contact = ?
+            WHERE id = ?
+                   """, (name, breed, age, description, city, contact, pet_id))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'mensagem': 'Pet atualizado com sucesso!'})
+
+
+@app.route('/pets/<int:pet_id>', methods=['DELETE'])
+def delete_animal(pet_id):
+    conn = conn_db()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM animals WHERE id = ?',(pet_id,))
+    conn.commit()
+    conn.close()
+    return jsonify({'mensagem': 'Pet deletado com sucesso!'})
     
 if __name__ == '__main__':
     app.run(debug=True)
